711e0bc43b8c43205499cb195fe6177f0da3c9b8
javaExactus <romulo.neves@exactus.com.br>
2025-03-20 13:45:49 -0300

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from "./componentes/container/container.component";
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, CommonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
 }
