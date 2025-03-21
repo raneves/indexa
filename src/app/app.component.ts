41ea653d18e1e80187ae830fdeb17a4043530b5b
javaExactus <romulo.neves@exactus.com.br>
2025-03-21 14:08:24 -0300

    terceio commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json'
 
 @Component({
   selector: 'app-root',
@@ -15,11 +24,37 @@ import { ContatoComponent } from './componentes/contato/contato.component';
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = ''
+
+  // Remove os acentos de uma string
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter(contato => {
+      // Compara os nomes sem acentuações
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
+    })
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter(contato => {
+      // Compara a letra inicial sem considerar acentuações
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(this.removerAcentos(letra).toLowerCase());
+    })
+  }
 }
