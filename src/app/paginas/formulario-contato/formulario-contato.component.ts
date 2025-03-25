4c549965ed11c43226d609b62f9e4a13e35ff855
javaExactus <romulo.neves@exactus.com.br>
2025-03-25 14:46:11 -0300

    rotas

---
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-formulario-contato',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    SeparadorComponent,
+    ReactiveFormsModule,
+    RouterLink
+  ],
+  templateUrl: './formulario-contato.component.html',
+  styleUrl: './formulario-contato.component.css'
+})
+export class FormularioContatoComponent {
+
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
+   })
+  }
+
+  salvarContato() {
+    console.log(this.contatoForm.value);
+  }
+
+  cancelar() {
+    console.log('Submissão cancelada');
+  }
+}
